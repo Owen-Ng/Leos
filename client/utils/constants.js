df81b14df928bd702b5c6e282415417a2d2980ff
Owen Ng <s.nigelowennlc@gmail.com>
2022-04-03 18:16:44 -0400

    First Blockchain Application

---
+++ client/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x330CB1E5e0C74756550E55E2223556C21aad513C'; // the address where the contract was deployed
